8dd783cf95f57f82a983f5967e5736965f9fc720
Harshil Chokshi <Harsh@Harshils-MacBook-Pro.local>
2017-01-07 14:10:28 -0500

    Modified package.json

--- database/mongoose.js
+++ database/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect = (process.env.MONGODB_URI || 'mongodb://localhost:27017');
+mongoose.connect = (process.env.MONGODB_URI || 'mongodb://localhost:27017/fc-barcelona-api');
 
 module.exports.mogoose = mongoose;
