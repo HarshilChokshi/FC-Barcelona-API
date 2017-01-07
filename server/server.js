dcb2b109e07b06b483f3f91f89dba7faf0986c8f
Harshil Chokshi <Harsh@Harshils-MacBook-Pro.local>
2017-01-07 16:08:38 -0500

    Testing GET players method

--- server/server.js
+++ server/server.js
@@ -9,7 +9,26 @@ const fillPlayers = require('./../database/initialize_database/fill_players.js')
 var app = express();
 var port = process.env.PORT || 3000;
 
-fillPlayers.addPlayers();
+
+//fillPlayers.addPlayers();
+
+app.use(bodyParser.json());
+
+app.get('/players', (req, res) => {
+  Player.find().then((players) => {
+    res.send({
+      status: 200,
+      playersCount: players.length;
+      players: players
+    });
+  }, (err) => {
+    res.status(400).send({
+      status: 400,
+      errorMessage: 'Unable to return data',
+      errorDetail: `Error: ${err}`
+    });
+  } );
+});
 
 app.listen(port,() => {
   console.log(`Server started up on port: ${port}`);
