77221b6e641d6ac0d2c09f864fc6ba23babb507a
Harshil Chokshi <Harsh@Harshils-MacBook-Pro.local>
2017-01-07 20:17:05 -0500

    Added players to database

--- database/mongoose.js
+++ database/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect = (process.env.MONGODB_URI || 'mongodb://localhost:27017/fc-barcelona-api');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/FCBarcelonaApp');
 
 module.exports.mogoose = mongoose;
--- server/server.js
+++ server/server.js
@@ -4,13 +4,13 @@ const bodyParser = require('body-parser');
 
 const mongoose = require('./../database/mongoose.js').mongoose;
 const Player = require('./../database/models/player.js').Player;
-const fillPlayers = require('./../database/initialize_database/fill_players.js')
+const fillPlayers = require('./../database/initialize_database/fill_players.js');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
 
-//fillPlayers.addPlayers();
+fillPlayers.addPlayers();
 
 app.use(bodyParser.json());
 
@@ -18,6 +18,7 @@ app.get('/players', (req, res) => {
   Player.find().then((players) => {
     res.send({
       status: 200,
+      playersCount: player.length,
       players: players
     });
   }, (err) => {
